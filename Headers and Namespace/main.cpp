39b881abbdae0e63dbd6273f0d7eddec3f7a1ae6
Fatih İver <fatih.iver@boun.edu.tr>
2018-02-08 14:17:32 +0300

    Add files via upload

---
+++ Headers and Namespace/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> // make standard facilities accessible
+#include <list>
+
+using namespace std; // make std names available without std::prefix
+
+int main()
+{
+    cout << "Headers and Namespace!" << endl;
+
+    string s {"C++ is a general-purpose programming language"}; // OK: string is std::string
+
+    return 0;
+}
